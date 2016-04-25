d7c5f5df1b922e896211da0036502b8e2bc5150c
Erik Goens <egoens@gmail.com>
2016-04-24 20:31:22 -0600

    simplifies webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,4 @@
-var path = require('path');
 module.exports = {
-  entry: ['webpack/hot/dev-server',path.resolve(__dirname, 'app/main.js')],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
   module: {
       loaders: [
           {
