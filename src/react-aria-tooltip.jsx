66b624c49a8f2cc3ffa6f56980841510bc015015
Erik Goens <egoens@gmail.com>
2018-07-25 08:49:21 -0500

    adds styled-components back into dev dependencies.

--- src/react-aria-tooltip.jsx
+++ src/react-aria-tooltip.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ToolTipContent from './tooltip-content'
-import styled from 'styled-components'
+import React from "react"
+import PropTypes from "prop-types"
+import ToolTipContent from "./tooltip-content"
+import styled from "styled-components"
 
-let tooltipIdCounter = 0;
+let tooltipIdCounter = 0
 
 class ReactARIAToolTip extends React.Component {
     constructor(props, context) {
@@ -19,33 +19,31 @@ class ReactARIAToolTip extends React.Component {
 
     componentWillMount() {
         const id = this.props.id || this.uniqueID("ra-tooltip-")
-        this.setState({id: id})
+        this.setState({ id: id })
     }
 
-    componentWillUnmount () {
-        this.timer && clearTimeout(this.timer);
-        this.timer = false;
+    componentWillUnmount() {
+        this.timer && clearTimeout(this.timer)
+        this.timer = false
     }
 
     startTimer() {
         const { duration } = this.props
-        this.timer = setTimeout(
-            () => this.setState({active: false}), duration
-        )
+        this.timer = setTimeout(() => this.setState({ active: false }), duration)
     }
 
     handleClick() {
         clearTimeout(this.timer)
-        this.setState({active: true})
+        this.setState({ active: true })
         this.startTimer()
     }
 
     handleMouseOver() {
-        this.setState({active: true})
+        this.setState({ active: true })
     }
 
     handleMouseLeave() {
-        this.setState({active: false})
+        this.setState({ active: false })
     }
 
     handleFocus() {
@@ -54,13 +52,13 @@ class ReactARIAToolTip extends React.Component {
 
     // create unique id so multiple tooltips can be used in the same view
     uniqueID(prefix) {
-        const id = ++tooltipIdCounter + '';
-        return prefix ? prefix + id : id;
+        const id = ++tooltipIdCounter + ""
+        return prefix ? prefix + id : id
     }
 
     // adds tooltip 'aria-describedby' attribute to child element
     addDescribedBy(tooltipID) {
-        return React.Children.map(this.props.children, (e) => {
+        return React.Children.map(this.props.children, e => {
             return React.cloneElement(e, {
                 "aria-describedby": tooltipID
             })
@@ -71,48 +69,45 @@ class ReactARIAToolTip extends React.Component {
         const { message, bgcolor, direction, className } = this.props
         const { active } = this.state
         let containerClass = `ra-tooltip-wrapper ${className}`
-        containerClass += (active) ? " active" : ""
+        containerClass += active ? " active" : ""
         const tooltipID = this.state.id
 
-        if (this.props.eventType == 'hover') {
+        if (this.props.eventType == "hover") {
             return (
                 <div
-                     onMouseOver={this.handleMouseOver.bind(this)}
-                     onMouseLeave={this.handleMouseLeave.bind(this)}
-                     role="tooltip"
-                     id={tooltipID}
-                     onFocus={this.handleFocus.bind(this)}
-                     className={containerClass}
+                    onMouseOver={this.handleMouseOver.bind(this)}
+                    onMouseLeave={this.handleMouseLeave.bind(this)}
+                    role="tooltip"
+                    id={tooltipID}
+                    onFocus={this.handleFocus.bind(this)}
+                    className={containerClass}
                 >
                     <ToolTipContent
                         message={message}
                         bgcolor={bgcolor}
                         direction={direction}
-                        active={active} />
-                     {this.addDescribedBy(tooltipID)}
+                        active={active}
+                    />
+                    {this.addDescribedBy(tooltipID)}
                 </div>
             )
         }
 
         return (
-            <div
-                 onClick={this.handleClick.bind(this)}
-                 role="tooltip"
-                 className={containerClass}
-            >
-                 <ToolTipContent
-                     message={message}
-                     bgcolor={bgcolor}
-                     direction={direction}
-                     active={active} />
-                 {this.addDescribedBy(tooltipID)}
+            <div onClick={this.handleClick.bind(this)} role="tooltip" className={containerClass}>
+                <ToolTipContent
+                    message={message}
+                    bgcolor={bgcolor}
+                    direction={direction}
+                    active={active}
+                />
+                {this.addDescribedBy(tooltipID)}
             </div>
         )
-
     }
 }
 
-ReactARIAToolTip.displayName = 'ReactARIAToolTip'
+ReactARIAToolTip.displayName = "ReactARIAToolTip"
 
 ReactARIAToolTip.defaultProps = {
     direction: "top",
@@ -122,18 +117,12 @@ ReactARIAToolTip.defaultProps = {
 }
 
 ReactARIAToolTip.propTypes = {
-    message: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.object,
-        PropTypes.element
-    ]).isRequired,
+    message: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.element])
+        .isRequired,
     direction: PropTypes.string,
-    duration: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]),
+    duration: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     children: PropTypes.node,
-    eventType: PropTypes.oneOf( ['hover', 'click'] ),
+    eventType: PropTypes.oneOf(["hover", "click"]),
     id: PropTypes.string,
     bgcolor: PropTypes.string
 }
@@ -141,7 +130,7 @@ ReactARIAToolTip.propTypes = {
 export default styled(ReactARIAToolTip)`
     position: relative;
     display: inline-block;
-    width: auto;
+    width: inherit;
 
     &.active {
         .ra-tooltip {
